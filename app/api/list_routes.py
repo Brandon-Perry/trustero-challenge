c2909c8dc3f73176e5e91634e49e72326ef34498
Brandon-Perry <brandonperryofficial@gmail.com>
2021-04-03 17:50:42 -0400

    tested and confirmed list_routes

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -1,38 +1,53 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from ..models import db, List
 
 list_routes = Blueprint('lists',__name__)
 
 
-#Get all lists
+#Get all lists - works
 @list_routes.route('/')
 def get_lists():
     all_lists = List.query.all()
-    return {'lists': [task_list.to_dict() for task_list in all_lists]}
+    print('------')
+    print(all_lists)
+    data = [task_list.to_dict() for task_list in all_lists]
+    print(data)
+    print('-----')
+    return jsonify(data)
 
-#Get single list
+#Get single list - works
 @list_routes.route('/<id>')
 def get_list(id):
     single_list = List.query.get(id)
-    return {'list':single_list.to_dict()}
+    data = single_list.to_dict()
+    return jsonify(data)
 
-#create list
+#create list - works
 @list_routes.route('/', methods=['POST'])
 def create_list():
     new_list = List()
     new_list.name = request.get_json().get('name')
     db.session.add(new_list)
+    db.session.commit()
 
-#edit list
+    data = new_list.to_dict()
+    return jsonify(data)
+
+#edit list - works
 @list_routes.route('/<id>', methods=['PUT'])
-def edit_list():
+def edit_list(id):
     edit_list = List.query.get(id)
     edit_list.name = request.get_json().get('name')
     db.session.commit()
 
-#Destroy list
+    data = edit_list.to_dict()
+    return jsonify(data)
+
+#Destroy list - works but needs confirmation
 @list_routes.route('<id>', methods=['DELETE'])
-def delete_list():
+def delete_list(id):
     the_list = List.query.get(id)
     db.session.delete(the_list)
     db.session.commit()
+
+    return 
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
 
   const getInfo = async () => {
-    let tasksRes = await fetch('/api/tasks/7', {
-      method:'DELETE',
-      headers: {'Content-Type': 'application/json'},
-      
+    let tasksRes = await fetch('/api/lists/4', {
+      method: 'DELETE',
+        
     })
+      
     console.log(tasksRes)
     let tasksJson = await tasksRes.json()
     console.log(tasksJson)
